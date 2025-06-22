c504e84f3b94394c0b26c169296451547f91d35e
lokeshtalla <tallalokesh.edu@gmail.com>
2025-06-22 11:20:52 +0530

    NodeJS Assignment-1

---
+++ NodeJS Assignment-1/nodejs-rest-api/data.js
@@ -0,0 +1,6 @@
+let users = [
+  { id: 1, name: 'Alice', email: 'alice@mail.com' },
+  { id: 2, name: 'Bob', email: 'bob@mail.com' }
+];
+
+module.exports = users;
---
+++ NodeJS Assignment-1/nodejs-rest-api/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+let users = require('./data');
+
+app.use(express.json());
+
+// GET all users
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+// GET user by ID
+app.get('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  user ? res.json(user) : res.status(404).send('User not found');
+});
+
+// POST create user
+app.post('/api/users', (req, res) => {
+  const newUser = {
+    id: users.length + 1,
+    name: req.body.name,
+    email: req.body.email
+  };
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+// PUT update user
+app.put('/api/users/:id', (req, res) => {
+  const user = users.find(u => u.id === parseInt(req.params.id));
+  if (user) {
+    user.name = req.body.name;
+    user.email = req.body.email;
+    res.json(user);
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+// DELETE user
+app.delete('/api/users/:id', (req, res) => {
+  const index = users.findIndex(u => u.id === parseInt(req.params.id));
+  if (index !== -1) {
+    users.splice(index, 1);
+    res.send('User deleted');
+  } else {
+    res.status(404).send('User not found');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
